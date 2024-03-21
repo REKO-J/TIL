0afda2effc0668e696b8e9603e2ac64e44c056e3
YeongHun Jo <117628869+REKO-J@users.noreply.github.com>
2024-03-21 23:27:35 +0900

    240321
    작성자, 수정, 삭제, 추천

--- FastAPI/myapi/database.py
+++ FastAPI/myapi/database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, MetaData
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -11,6 +11,16 @@ engine = create_engine(
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)  # autocommit=True -> rollback 불가능!
 
 Base = declarative_base()
+
+# MetData 클래스 - 데이터베이스의 프라이머리 키, 유니크 키, 인덱스 키 등의 이름 규칙을 새롭게 정의(수동)
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+Base.metadata = MetaData(naming_convention=naming_convention)
 
 
 # FastAPI의 "Dependency Injection"
--- FastAPI/myapi/models.py
+++ FastAPI/myapi/models.py
@@ -1,16 +1,35 @@
-from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey
+from sqlalchemy import Column, Integer, String, Text, DateTime, ForeignKey, Table
 from sqlalchemy.orm import relationship
 
 from database import Base
 
+question_voter = Table(
+    'question_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('question_id', Integer, ForeignKey('question.id'), primary_key=True)
+)
 
-class Question(Base):           # Base Class 상속
+
+class Question(Base):  # Base Class 상속
     __tablename__ = "question"  # 테이블 이름 정의
 
-    id = Column(Integer, primary_key=True)    # primary_key
+    id = Column(Integer, primary_key=True)  # primary_key
     subject = Column(String, nullable=False)  # nullable=False -> null값 허용 x
     content = Column(Text, nullable=False)
     create_date = Column(DateTime, nullable=False)
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="question_users")
+    modify_date = Column(DateTime, nullable=True)
+    voter = relationship('User', secondary=question_voter, backref='question_voters')
+
+
+answer_voter = Table(
+    'answer_voter',
+    Base.metadata,
+    Column('user_id', Integer, ForeignKey('user.id'), primary_key=True),
+    Column('answer_id', Integer, ForeignKey('answer.id'), primary_key=True)
+)
 
 
 class Answer(Base):
@@ -21,6 +40,10 @@ class Answer(Base):
     create_date = Column(DateTime, nullable=False)
     question_id = Column(Integer, ForeignKey("question.id"))  # ForeignKey
     question = relationship("Question", backref="answers")
+    user_id = Column(Integer, ForeignKey("user.id"), nullable=True)
+    user = relationship("User", backref="answer_users")
+    modify_date = Column(DateTime, nullable=True)
+    voter = relationship('User', secondary=answer_voter, backref='answer_voters')
 
 
 class User(Base):
